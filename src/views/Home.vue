65b2d0769ac4385ddfaf8ddc4981db973887c674
pavithra91 <dev.pavithraj@gmail.com>
2022-08-18 16:28:40 +0530

    com

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,222 +1,49 @@
 <template>
-  <div>
+  <v-container>
     <v-banner>
-      <v-img :src="require('../assets/img/banner/1.png')" class="my-3" max-width="100%" max-height="100%" />
-    </v-banner>
-
-    <div class="topfund">
-      <v-layout row justify-center class="topfund-container">
-        <v-flex md3>
+    <v-img :src="require('../assets/img/banner/1.png')" class="my-2" max-width="100%" max-height="100%" />
+</v-banner>
+   <v-layout row justify-center>
+      <v-row class="my-3">
+        <v-col v-for="n in 3"
+      :key="n">
           <TopFundRaiser />
-        </v-flex>
-        <v-flex md3>
-          <TopFundRaiser />
-        </v-flex>
-        <v-flex md3>
-          <TopFundRaiser />
-        </v-flex>
-      </v-layout>
-    </div>
+        </v-col>
+      </v-row>
+   </v-layout>
 
-    <v-divider style="margin-top: 50px;"></v-divider>
-
-
-
-
-    <div class="category-container">
-
+   <v-layout row justify-center>
       <v-row>
+        <v-col offset-lg="2" offset-md="2">
+              <label class="text-h2"> Browse fundraisers by category </label>
+        </v-col>
+      </v-row>
+   </v-layout>
+      <v-layout row justify-center>
+      <v-row>
+        <v-col offset-lg="2" offset-md="2">
 
-        <v-col></v-col>
+            <v-img :src="require('../assets/img/icons/icon_vertical_ medical.svg')" width="100" height="100" />
+
+            Medical & Health
+        </v-col>
 
         <v-col>
-          <p class="font-weight-black" style="font-size: xx-large; padding-left: 100px; padding-top: 20px;">
-            Browse fundraisers by category
-          </p>
+            <v-img :src="require('../assets/img/icons/icon_vertical_ memorial.svg')" width="100" height="100" />
+            Memorials & Funerals
         </v-col>
-        <v-col></v-col>
+
+                <v-col>
+            <v-img :src="require('../assets/img/icons/icon_vertical_ NP.svg')" width="100" height="100" />
+        </v-col>
+
+                <v-col>
+            <v-img :src="require('../assets/img/icons/icon_vertical_ medical.svg')" width="100" height="100" />
+        </v-col>
 
       </v-row>
-      <v-layout row justify-center class="category-icon-div">
-        <v-row>
-          <v-spacer></v-spacer>
-          <v-spacer></v-spacer>
-          <v-col>
-            <div style="padding-left: 10px">
-              <v-img :src="require('../assets/img/icons/icon_vertical_ medical.svg')" width="100" height="100" />
-            </div>
-            <div>
-              <v-btn plain style="padding: 0; margin: 0; color: black;">Medical & Health</v-btn>
-            </div>
-
-          </v-col>
-          <v-col>
-            <div style="padding-left: 25px">
-              <v-img :src="require('../assets/img/icons/icon_vertical_ memorial.svg')" width="100" height="100" />
-            </div>
-            <div>
-              Memorials & Funerals
-            </div>
-
-          </v-col>
-          <v-col>
-            <div style="padding-left: 10px">
-              <v-img :src="require('../assets/img/icons/icon_vertical_ NP.svg')" width="100" height="100" />
-            </div>
-            <div>
-              NonProfit/Charity
-            </div>
-
-          </v-col>
-          <v-col>
-            <div style="padding-left: 10px">
-              <v-img :src="require('../assets/img/icons/icon_vertical_ school.svg')" width="100" height="100" />
-            </div>
-            <div>
-              Schools & Education
-            </div>
-          </v-col>
-          <v-spacer></v-spacer>
-        </v-row>
-
-      </v-layout>
-      <v-layout row justify-center class="category-icon-div">
-        <v-row>
-          <v-spacer></v-spacer>
-          <v-spacer></v-spacer>
-          <v-col>
-            <div style="padding-left: 1px">
-              <v-img :src="require('../assets/img/icons/icon_vertical_ pets.svg')" width="100" height="100" />
-            </div>
-            <div>
-              Pets & Animals
-            </div>
-
-          </v-col>
-          <v-col>
-            <div style="padding-left: 1px">
-              <v-img :src="require('../assets/img/icons/icon_vertical_ disaster.svg')" width="100" height="100" />
-            </div>
-            <div>
-              Disaster Relief
-            </div>
-
-          </v-col>
-          <v-col>
-            <div style="padding-left: 25px">
-              <v-img :src="require('../assets/img/icons/icon_vertical_club.svg')" width="100" height="100" />
-            </div>
-            <div>
-              Clubs & Community
-            </div>
-
-          </v-col>
-          <v-col>
-            <div style="padding-left: 5px">
-              <v-img :src="require('../assets/img/icons/icon_vertical_ sport.svg')" width="100" height="100" />
-            </div>
-            <div>
-              Sports & Teams
-            </div>
-          </v-col>
-          <v-spacer></v-spacer>
-        </v-row>
-
-      </v-layout>
-
-      <v-layout row justify-center class="category-icon-div">
-        <v-row>
-          <v-spacer></v-spacer>
-          <v-spacer></v-spacer>
-          <v-col>
-            <div style="padding-left: 10px">
-              <v-img :src="require('../assets/img/icons/icon_vertical_ creative.svg')" width="100" height="100" />
-            </div>
-            <div>
-              Creative Projects
-            </div>
-
-          </v-col>
-          <v-col>
-            <div style="padding-left: 10px">
-              <v-img :src="require('../assets/img/icons/icon_vertical_ fun.svg')" width="100" height="100" />
-            </div>
-            <div style="padding-left: 20px">
-              Events
-            </div>
-
-          </v-col>
-          <v-col>
-            <div style="padding-left: 10px">
-              <v-img :src="require('../assets/img/icons/icon_vertical_ kids.svg')" width="100" height="100" />
-            </div>
-            <div style="padding-left: 10px">
-              Kids & Family
-            </div>
-
-          </v-col>
-          <v-col>
-            <div style="padding-left: 10px">
-              <v-img :src="require('../assets/img/icons/icon_vertical_ LGBT.svg')" width="100" height="100" />
-            </div>
-            <div style="padding-left: 35px">
-              LGBT
-            </div>
-          </v-col>
-          <v-spacer></v-spacer>
-        </v-row>
-      </v-layout>
-    </div>
-
-
-    <div>
-      <v-layout style="margin-top: 100px;margin-bottom: 100px;">
-        <v-row>
-          <v-col cols="2"></v-col>
-          <v-col>
-            <v-row>
-              <v-col cols="4" style="margin-left: 30px;">
-                <v-img src="../assets/img/icons/pc_optimized.png" width="100px" height="100px"></v-img>
-              </v-col>
-              <v-col>
-                <v-row  style="margin-top: 5px;">
-                  <span style="font-weight: bold; font-size: 20px;">Create</span>
-                </v-row>
-                <v-row>Anyone can start a Charity campaign for FREE.</v-row>            
-              </v-col>
-            </v-row>
-          </v-col>
-          <v-col>
-                        <v-row>
-              <v-col cols="4" style="margin-left: 30px;">
-                <v-img src="../assets/img/icons/megaphone-2_optimized.png" width="100px" height="100px"></v-img>
-              </v-col>
-              <v-col>
-                <v-row  style="margin-top: 5px;">
-                  <span style="font-weight: bold; font-size: 20px;">Share</span>
-                </v-row>
-                <v-row>Easily share your campaign and reach more people.</v-row>            
-              </v-col>
-            </v-row>
-          </v-col>
-          <v-col>
-                                    <v-row>
-              <v-col cols="4"  style="margin-left: 30px;">
-                <v-img src="../assets/img/icons/wallet_optimized.png" width="100px" height="100px"></v-img>
-              </v-col>
-              <v-col>
-                <v-row  style="margin-top: 5px;">
-                  <span style="font-weight: bold; font-size: 20px;">Share</span>
-                </v-row>
-                <v-row>Receive and access your money right away.</v-row>            
-              </v-col>
-            </v-row>
-          </v-col>
-          <v-col cols="2"></v-col>
-        </v-row>
-      </v-layout>
-    </div>
-  </div>
+   </v-layout>
+     </v-container>
 </template>
 
 <script>
